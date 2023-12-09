0c17bb2353d5177fda452a80c3c71d273bf2b8c2
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-09 17:44:18 +0530

    Update Pre-Requisites.py

--- Pre-Requisites.py
+++ Pre-Requisites.py
@@ -9,21 +9,6 @@ subprocess.run(["pip", "install", "webdriver_manager"])
 # Upgrade webdriver_manager
 subprocess.run(["pip", "install", "--upgrade", "webdriver_manager"])
 
-# Install curl
-subprocess.run(["apt", "install", "curl", "-y"])
-
-# Download Brave keyring with curl
-subprocess.run(["curl", "-fsSLo", "/usr/share/keyrings/brave-browser-archive-keyring.gpg", "https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"])
-
-# Add Brave repository to sources.list
-subprocess.run(["echo", "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main", ">", "/etc/apt/sources.list.d/brave-browser-release.list"])
-
-# Update apt
-subprocess.run(["apt", "update"])
-
-# Install Brave browser
-subprocess.run(["apt", "install", "brave-browser"])
-
 # Install pyppeteer
 subprocess.run(["pip", "install", "pyppeteer"])
 
